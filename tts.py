25420a5a3c86cbc82c6c2865d840703ea6c4902f
OlamideLanre <lanreprincess@gmail.com>
2024-12-02 11:11:30 +0100

    intro + fixed bug

--- tts.py
+++ tts.py
@@ -19,53 +19,52 @@ spelling_bee_words = [
     "diabolical",
     "effervescent",
     "facetious",
-    "gregarious",
+    "aerodynamic",
     "haphazard",
     "incongruous"
 ]
-correct=[]
-missed=[]
-said= None
-count=0
+
 # Initialize the TTS engine
 engine = pyttsx3.init()
-    # Set properties (optional)
 engine.setProperty('rate', 150)  # Speed of speech
 engine.setProperty('volume', 0.9)  # Volume level (0.0 to 1.0)
 
-def usersGuess(users_spelling,word, index):
-    if users_spelling == word[index]:
-        count=count+1
-        engine.say("Correct!")
-        engine.runAndWait()
-        
-        # appended= correct.append(users_spelling)
-        # if appended in spelling_bee_words:
-        #     said= True
-        #     if said==True:
-        # SayWord()
-        if count== len(spelling_bee_words):
-            engine.say("game over")
-            engine.runAndWait()
-    else:
-        engine.say("Incorrect")
-        engine.runAndWait()
-
 def SayWord():
-    while True:
-        random_number = random.randint(1, 19)
-        print(random_number)
-        for index in range(len(spelling_bee_words)):
-            if random_number == index:
-                # Convert text to speech
-                engine.say(spelling_bee_words[index])
-                engine.runAndWait()
-                users_spelling= input("spell the word: ")
-                usersGuess(users_spelling= users_spelling,word= spelling_bee_words, index=index)
-               
+    count = 19
+    score = 0
+    missed = 0
+    while spelling_bee_words:
+        word = random.choice(spelling_bee_words)  # Select a random word from the list
+        engine.say(word)
+        engine.runAndWait()
+        users_spelling = input("Spell the word: ")
+        if users_spelling == word:
+            score += 1
+            engine.say("Correct!")
+            engine.runAndWait()
+            spelling_bee_words.remove(word)
+        else:
+            engine.say("Incorrect")
+            engine.runAndWait()
+            missed += 1
+            spelling_bee_words.remove(word)
+        
+        if spelling_bee_words:
+            print("Game continues")
+        else:
+            engine.say(f"Game over, you scored {score} out of {count} and missed {missed}")
+            engine.runAndWait()
+            break
 
-SayWord()
-
-
+print("******BEE-RILLIANT*****")
+user= input("Please register by entering your name: ")
+print("******YOU READY? LETS GO!*****")
+option=input("yes/no: ")
+if option=="yes":
+    print(f"Welcome {user} to BEE-RILLIANT spelling bee!")
+    print("LETS GO!")
+    SayWord()
+else:
+    print("ok bye")
 
 
